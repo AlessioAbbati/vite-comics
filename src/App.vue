230ec029c4346f7f98f9a5075df3e3add517c43d
AlessioAbbati <alex_117@live.it>
2023-05-18 16:16:21 +0200

    base

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppContent from './components/AppContent.vue'
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue'
+export default {
+  components: { AppHeader, AppContent, AppFooter },
+
+}
+</script>
+
+<template>
+  <app-header />
+  <main>
+    <app-content />
+  </main>
+  <app-footer />
+</template>
+
+<style lang="scss">
+ @use './assets/styles/general.scss'
+
+</style>
+
