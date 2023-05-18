2169acb1add281f05266b6125b01df9110f8b859
AlessioAbbati <alex_117@live.it>
2023-05-18 18:53:12 +0200

    add footer-mid

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -31,7 +31,52 @@ export default {};
         </section>
         <section class="footer-mid">
             <div class="contacts">
+               <div class="links">
+                <div>
+                    <ul>
+                        <li class="title">DC COMICS</li>
+                        <li><a href="">Character</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+                    <ul>
+                        <li class="title">SHOP</li>
+                        <li><a href="">Shop DC</a></li>
+                        <li><a href="">Shop DC Collectibles</a></li>
+                    </ul>
+                    
+                </div>
+                <ul>
+                        <li class="title">DC</li>
+                        <li><a href="">Terms Of Use</a></li>
+                        <li><a href="">Privacy policy (New)</a></li>
+                        <li><a href="">Ad Choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certeficates</a></li>
+                        <li><a href="">Ratings</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                    <ul>
+                        <li class="title">SITES</li>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power Visa</a></li>
+                    </ul>
 
+               </div>
+               <div class="image">
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+               </div>
             </div>
         </section>
         <section class="footer-bot">
@@ -74,7 +119,33 @@ export default {};
 
 .footer-mid {
     background-image: url('../assets/img/footer-bg.jpg');
-    height: 16rem;
+    overflow: hidden;
+    .contacts {
+        height: 16rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        max-width: 1200px;
+        margin: auto;
+    }
+    .links {
+       display: flex;
+       .title {
+        font-weight: 700;
+        color: white;
+        margin-bottom: .5rem;
+        margin-top: .5rem;
+       }
+
+       li {
+        list-style: none;
+        a {
+            text-decoration: none;
+            color: gray;
+            font-size: .7rem;
+        }
+       }
+    }
 }
 
 .footer-bot {
