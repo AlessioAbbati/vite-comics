f399137afccc82629ceb31c1f6eb7141e71bc26f
AlessioAbbati <alex_117@live.it>
2023-05-19 17:44:43 +0200

    add appcomics and v-for

---
+++ src/components/AppComics.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+    props: {
+        urlImg: String,
+        series: String,
+        price: String,
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <img :src="urlImg" :alt="series" />
+        <h3>{{ series }}</h3>
+    </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
